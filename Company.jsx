ee84beef922ea2b769606214acb879e7ee1ea5cd
jamunaarunachalam <110532082+jamunaarunachalam@users.noreply.github.com>
2024-06-14 13:10:03 +0530

    Add files via upload

---
+++ Company.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export default function Company() {
+  return (
+    <div>
+    <div className='heading1'>
+    <h1>E-commerce Site</h1>
+    <h2>Top Products</h2>
+    </div>
+    <div className='samsung'>
+     <h3>SAMSUNG</h3>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzKvlSjq_0MrP6UvpqQIIq4BTk5qcuIY6qUw&s" height="100px"></img>
+     <img src="https://img.global.news.samsung.com/in/wp-content/uploads/2018/07/3.jpg" height="100px"></img>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQW-TWdRu2tg5RjBWZiGGi7Xtb1xPtsjqf7MQ&s" height="100px"></img>
+    </div>
+    <div className='samsung'>
+    <h3>NIKE</h3>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvGZnJHAiVmw0OROUhlzseaYhMZ3HoyhZTKw&s" height="100px"></img>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzG0HnSHTUt-8FYnRof9lUYgSrugk1IAN9wA&s" height="100px"></img>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDpSDpN_uCgaQ92-V7Y_ZyLJx7yCGONeo4NQ&s" height="100px"></img>
+    </div>
+    <div className='samsung'>
+    <h3>APPLE</h3>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQklXIH9_0GC-5Q6RuXjSADb_9TEHLveyyt_Q&s" height="100px"></img>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKiQdYm9OcSRlZ8Zm-C8tUa-kQlwx_pSWm2w&s" height="100px"></img>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2bhMMwCbdAWrg1Bryjl2xUI67JhEqXTiSEw&s" height="100px"></img>
+    </div>
+    <div className='samsung'>
+    <h3>SAMSUNG</h3>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzrVEdoTgxW8rGlfe6W_Z5LXqLpw6KCRQ_vQ&s" height="100px"></img>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7kx9zKesFgtixGFAaum-9ZQoiHf40pOv-iQ&s" height="100px"></img>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJCrF93IHDF2XCLXSzdojIqjcyBZ3AEWXObA&s" height="100px"></img>
+    </div>
+    <div className='samsung'>
+    <h3>SAMSUNG</h3>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHdQSGUtlr4XlKDoqWNNE0-7qZha26xVkQ0JTRMoLM7acTWfKSo-teS10uQSZxJuo8260&usqp=CAU" height="100px"></img>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkBaFQSJHty52HCQ2imh1k6SHPtSrIxIDE4A&s" height="100px"></img>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ3syunjmZgXfzg-FEt2kIW38jEDmZM8YIi6f8HgTflwHXmBu17CZXTk6qvBwqbLVN9q4&usqp=CAU" height="100px"></img>
+    </div>
+    </div>
+  )
+}
